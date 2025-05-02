f4ff768bc039b8fb30fe3ce70968c045ea1936e3
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2025-05-02 18:05:10 +0200

    fix error in graphical representation for network representation

--- src/main/kotlin/it/unibo/alchemist/boundary/swingui/effect/impl/DrawNetwork.kt
+++ src/main/kotlin/it/unibo/alchemist/boundary/swingui/effect/impl/DrawNetwork.kt
@@ -27,6 +27,7 @@ class DrawNetwork: Effect {
         val is5GAntenna = node.getConcentration(SimpleMolecule("5gAntenna")).toBoolean()
         val isStation = node.getConcentration(SimpleMolecule("station")).toBoolean()
         if (!isStation && !is5GAntenna) {
+            val isMoleculeDefined = !node.getConcentration(leader).toDouble().isNaN()
             val imLeader = node.getConcentration(iAmLeader).toBoolean()
             val imRelay = node.getConcentration(iAmRelay).toBoolean()
             val myLeader = node.getConcentration(leader).toInt()
@@ -34,34 +35,36 @@ class DrawNetwork: Effect {
             val intraClusterRelay = node.getConcentration(intraClusterRelay).toInt()
             val myId = node.id
             val myPosition = wormhole.getViewPoint(environment.getPosition(node))
-Double.NaN
+
             // Line configuration
             graphics.color = colorSummary
             graphics.stroke = BasicStroke(1.0f)
 
-            if (myLeader != myId) {
-                // I am not the leader of the cluster.
-                if (intraClusterRelay != myLeader) {
-                    // I have an intermediate to reach the leader.
-                    val intermediate = environment.getNodeByID(intraClusterRelay)
-                    val intermediatePosition = wormhole.getViewPoint(environment.getPosition(intermediate))
-                    graphics.drawLine(myPosition.x, myPosition.y, intermediatePosition.x, intermediatePosition.y)
-                    Arrow().drawArrowHead(graphics, myPosition, intermediatePosition)
+            if (isMoleculeDefined) {
+                if (myLeader != myId) {
+                    // I am not the leader of the cluster.
+                    if (intraClusterRelay != myLeader) {
+                        // I have an intermediate to reach the leader.
+                        val intermediate = environment.getNodeByID(intraClusterRelay)
+                        val intermediatePosition = wormhole.getViewPoint(environment.getPosition(intermediate))
+                        graphics.drawLine(myPosition.x, myPosition.y, intermediatePosition.x, intermediatePosition.y)
+                        Arrow().drawArrowHead(graphics, myPosition, intermediatePosition)
 
-                } else {
-                    // I directly communicate with the leader.
-                    val leader = environment.getNodeByID(myLeader)
-                    val leaderPosition = wormhole.getViewPoint(environment.getPosition(leader))
-                    graphics.drawLine(myPosition.x, myPosition.y, leaderPosition.x, leaderPosition.y)
-                    Arrow().drawArrowHead(graphics, myPosition, leaderPosition)
+                    } else {
+                        // I directly communicate with the leader.
+                        val leader = environment.getNodeByID(myLeader)
+                        val leaderPosition = wormhole.getViewPoint(environment.getPosition(leader))
+                        graphics.drawLine(myPosition.x, myPosition.y, leaderPosition.x, leaderPosition.y)
+                        Arrow().drawArrowHead(graphics, myPosition, leaderPosition)
 
-                }
-            } else if (imLeader && myRelay != myId) {
-                val relayCandidate = environment.getNodeByID(myRelay)
-                if (relayCandidate.getConcentration(SimpleMolecule("imRelay")).toBoolean()) {
-                    val relayPosition = wormhole.getViewPoint(environment.getPosition(relayCandidate))
-                    graphics.drawLine(myPosition.x, myPosition.y, relayPosition.x, relayPosition.y)
-                    Arrow().drawArrowHead(graphics,myPosition, relayPosition)
+                    }
+                } else if (imLeader && myRelay != myId) {
+                    val relayCandidate = environment.getNodeByID(myRelay)
+                    if (relayCandidate.getConcentration(SimpleMolecule("imRelay")).toBoolean()) {
+                        val relayPosition = wormhole.getViewPoint(environment.getPosition(relayCandidate))
+                        graphics.drawLine(myPosition.x, myPosition.y, relayPosition.x, relayPosition.y)
+                        Arrow().drawArrowHead(graphics, myPosition, relayPosition)
+                    }
                 }
             }
         }
