228fcc3f9231e20291ac1725fcb8437814fd773b
Martina Baiardi <martina.baiardi4@studio.unibo.it>
2025-05-03 12:41:00 +0200

    final touches on charts

--- process.py
+++ process.py
@@ -649,25 +649,25 @@ if __name__ == '__main__':
         df = pd.read_csv("data/metric_data.csv")
         viridis = plt.colormaps['viridis']
         
-        plt.figure(figsize=(8, 6))
+        plt.figure(figsize=(7, 3.5))
         widthOfLine = 4
-        plt.plot(df["x"], df["y_wifi"], label="Wi-Fi", linewidth=widthOfLine, color=viridis(0.9))
+        plt.plot(df["x"], df["y_wifi"], label="Wi-Fi", linewidth=widthOfLine, color=viridis(0.4))
         plt.plot(df["x"], df["y_aprs"], label="APRS", linewidth=widthOfLine, color=viridis(0.65))
-        plt.plot(df["x"], df["y_lora"], label="LoraWAN", linewidth=widthOfLine, color=viridis(0.4))
+        plt.plot(df["x"], df["y_lora"], label="LoraWAN", linewidth=widthOfLine, color=viridis(0.9))
         plt.plot(df["x"], df["y_midband5g"], label="Midband 5G", linewidth=widthOfLine, color=viridis(0.1))
         
-        def addHorizontalAxe(yValue, label, position=6000):
+        def addHorizontalAxe(yValue, label, yOffset, position=6000):
             plt.axhline(y=yValue, color='black', linestyle='--')
-            plt.text(x=position, y=yValue + 0.00000001, s=label, color='black', fontsize=10, verticalalignment='bottom' )
+            plt.text(x=position, y=yValue+yOffset, s=label, color='black', verticalalignment='bottom' )
         
-        addHorizontalAxe(5.0, "Full-HD video (~5Mbps)", 1.5)
-        addHorizontalAxe(2.5, "HD-Ready audio (~2.5Mbps)", 1.5)
-        addHorizontalAxe(0.320, "High-Quality audio (~320kbps)", 1.5)
-        addHorizontalAxe(0.064, "Low-Quality audio (~64kbps)")
-        addHorizontalAxe(0.032, "Speech only audio (~32kbps)")
-        addHorizontalAxe(0.100, "Rich text data (~100kbps)", 1.5)
-        addHorizontalAxe(0.001, "Position, Identification, Direction (1kbps)", 1.5)
-        addHorizontalAxe(0.0001, "Keep Alive Message (10bps)", 1.5)
+        addHorizontalAxe(5.0, "Full-HD video (~5Mbps)", 0, 1.5)
+        addHorizontalAxe(2.5, "HD-Ready video (~2.5Mbps)", -1.55, 1.5)
+        addHorizontalAxe(0.320, "High-Quality audio (~320kbps)", 0, 1.5)
+        #addHorizontalAxe(0.064, "Low-Quality audio (~64kbps)")
+        addHorizontalAxe(0.032, "Speech only audio (~32kbps)", -0.02, 1.5)
+        addHorizontalAxe(0.100, "Rich text data (~100kbps)", 0, 1.5)
+        addHorizontalAxe(0.001, "Position, ID, Direction (1kbps)", 0, 1.5)
+        addHorizontalAxe(0.0001, "Keep Alive Message (10bps)", 0, 1.5)
                 
         plt.yscale('symlog', linthresh=0.001)
         plt.xscale("log")
